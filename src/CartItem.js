b872d2693923499bc3c6585412b22a6780b10fed
Samiran Roy <samiranroy0407@gmail.com>
2021-04-03 05:16:12 +0530

    first commit

---
+++ src/CartItem.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class CartItem extends React.Component{
+
+    constructor(){
+        super();
+
+        this.state = {
+            price: 999,
+            title: 'Puma 113',
+            qty: 1,
+            img: ''
+        }
+    }
+    render() {
+
+        const {price, title, qty} = this.state;
+
+        return (
+            <div className="cart-item">
+            <div className="left-block">
+                <img className="cart-image" src=""></img>
+            </div>
+            <div className="right-block">
+                <div>{price}</div>
+                <div>{title}</div>
+                <div>{qty}</div>
+            </div>
+            <div className="cart-item-actions">
+                <div>
+                    <img className="action-icons" src="https://www.flaticon.com/premium-icon/icons/svg/3303/3303893.svg" alt="plus"></img>
+                    <img className="action-icons" src="https://www.flaticon.com/premium-icon/icons/svg/2985/2985073.svg" alt="minus"></img>
+                    <img className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/3096/3096673.svg?token=exp=1617407128~hmac=e0aad1d8552ee358d810248f4c5f0188" alt="delete"></img>
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default CartItem;
