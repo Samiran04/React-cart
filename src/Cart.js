7cafc1ddbc906e04c03b3f74c3dc792eecd56d1e
Samiran Roy <samiranroy0407@gmail.com>
2021-04-05 23:33:32 +0530

    different cart items rendered

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,45 @@ import React from 'react';
 import CartItem from './CartItem';
 
 class Cart extends React.Component {
+
+    constructor() {
+
+        super();
+
+        this.state = {
+            products: [
+                {
+                    price: 999,
+                    title: 'Mobile',
+                    qty: 1,
+                    img: '',
+                    id: 1
+                },
+                {
+                    price: 400,
+                    title: 'Shoes',
+                    qty: 5,
+                    img: '',
+                    id: 2
+                },
+                {
+                    price: 777,
+                    title: 'Dress',
+                    qty: 9,
+                    img: '',
+                    id: 3
+                }
+            ]
+        }
+    }
+
     render() {
+        const {products} = this.state;
         return (
             <div className="cart">
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+                {products.map((product) => {
+                    return (<CartItem product = {product} key = {product.id} />)
+                })}
             </div>
         );
     }
